f59b36478ddb404bc491dd032c2eb9d50def32cf
florianingerl <imelflorianingerl@gmail.com>
2023-04-19 08:47:10 +0200

    Initialized the project with maven

---
+++ createapp.bat
@@ -0,0 +1,6 @@
+mvn archetype:generate ^
+-DgroupId=com.johannes.propra ^
+-DartifactId=snakeapp ^
+-DarchetypeArtifactId=maven-archetype-quickstart ^
+-DarchetypeVersion=1.4 ^
+-DinteractiveMode=false
